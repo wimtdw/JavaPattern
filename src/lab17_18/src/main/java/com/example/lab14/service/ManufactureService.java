47872b7e7abf31310deb9a9e177c2be809fd7312
wimtdw <153401722+wimtdw@users.noreply.github.com>
2024-05-24 21:07:24 +0300

    added lab17-18

---
+++ src/lab17_18/src/main/java/com/example/lab14/service/ManufactureService.java
@@ -0,0 +1,35 @@
+package com.example.lab14.service;
+
+import java.util.List;
+
+import com.example.lab14.entity.Manufacture;
+import com.example.lab14.repository.ManufactureRepository;
+import com.example.lab14.specs.ManufactureSpec;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ManufactureService {
+    private final ManufactureRepository manufactureRepository;
+
+    public Long createManufacture(Manufacture manufacture) {
+        manufactureRepository.save(manufacture);
+        return manufacture.getId();
+    }
+
+    public List<Manufacture> getManufacture() {
+        return manufactureRepository.findAll();
+    }
+
+    public List<Manufacture> getManufacturesSortedBy(String attributeName) {
+        Sort sort = Sort.by(Sort.Direction.ASC, attributeName);
+        return manufactureRepository.findAll(ManufactureSpec.sortBy(attributeName), sort);
+    }
+
+    public void deleteManufacture(Long id) {
+        manufactureRepository.deleteById(id);
+    }
+}
---
+++ src/lab17_18/src/main/java/com/example/lab14/service/WorkerService.java
@@ -0,0 +1,37 @@
+package com.example.lab14.service;
+
+import java.util.List;
+
+import com.example.lab14.entity.Manufacture;
+import com.example.lab14.entity.Worker;
+import com.example.lab14.repository.WorkerRepository;
+import com.example.lab14.specs.ManufactureSpec;
+import com.example.lab14.specs.WorkerSpec;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class WorkerService {
+    private final WorkerRepository workerRepository;
+
+    public Long createWorker(Worker worker) {
+        workerRepository.save(worker);
+        return worker.getId();
+    }
+
+    public List<Worker> getWorkers() {
+        return workerRepository.findAll();
+    }
+
+    public List<Worker> getWorkersSortedBy(String attributeName) {
+        Sort sort = Sort.by(Sort.Direction.ASC, attributeName);
+        return workerRepository.findAll(WorkerSpec.sortBy(attributeName), sort);
+    }
+
+    public void deleteWorker(Long id) {
+        workerRepository.deleteById(id);
+    }
+}
